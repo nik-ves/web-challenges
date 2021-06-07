487f5672acf2e0526528161b537953a48c12d3af
nik-ves <nik-ves@hotmail.com>
2021-06-08 01:06:35 +0200

    Projects added

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-// JS
+const projects = [
+	{
+		name: 'four-card-feature-section'
+	},
+	{
+		name: 'clipboard-landing-page'
+	}
+];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name }) => {
+	const listItem = document.createElement('li');
+
+	listItem.innerHTML = `
+		<a href="/${name}/index.html" target="_blank">
+			<img src="/${name}/design/desktop-design.jpg" alt="${name}" />
+			<p>${formatProjectName(name)}</p>
+		</a>
+
+		<div class="links-container">
+			<a href="https://github.com/nik-ves/${name}" class="black">
+				<i class="fab fa-github"></i>
+			</a>
+
+			<a href="/${name}/index.html" class="black">
+				<i class="fas fa-eye"></i>
+			</a>
+		</div>
+	`;
+
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word
+		.slice(1)).join(' ');
+};
